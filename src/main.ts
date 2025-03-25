98d8ee9be9d839cc309b72973e5e64de2827be27
Parkchangwoo <whwh3560@gmail.com>
2025-03-25 10:30:39 +0900

    feat: tcp test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
+import { type MicroserviceOptions } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new ConfigService();
+  const options = config.get('service');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    options,
+  );
+
+  await app.listen();
+
+  Logger.log(
+    `🚀 Application is running on: TCP ${JSON.stringify(options)}`,
+    'bootstrap-msa',
+  );
 }
 bootstrap();
