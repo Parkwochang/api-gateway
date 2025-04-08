bb4c0d8aadfc49f1673e9c5dac9df5fcb7e376cc
Turbobot <turbobot@vercel.com>
2025-04-09 00:51:38 +0900

    feat(create-turbo): apply official-starter transform

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), OrderModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
